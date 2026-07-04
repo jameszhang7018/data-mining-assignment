3be7f67cac038f2ad1478653585515c628428082
jameszhang7018 <jameszhang18@gmail.com>
2026-07-04 18:17:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -26,6 +26,31 @@ class CustomerData(BaseModel):
     MonthlyCharges: float
     TotalCharges: float
 
+    class Config:
+        schema_extra = {
+            "example": {
+                "gender": "Female",
+                "SeniorCitizen": 0,
+                "Partner": "Yes",
+                "Dependents": "No",
+                "tenure": 24,
+                "PhoneService": "Yes",
+                "MultipleLines": "No",
+                "InternetService": "Fiber optic",
+                "OnlineSecurity": "No",
+                "OnlineBackup": "Yes",
+                "DeviceProtection": "No",
+                "TechSupport": "No",
+                "StreamingTV": "Yes",
+                "StreamingMovies": "Yes",
+                "Contract": "Month-to-month",
+                "PaperlessBilling": "Yes",
+                "PaymentMethod": "Electronic check",
+                "MonthlyCharges": 89.9,
+                "TotalCharges": 2157.6
+            }
+        }
+
 # 加载模型
 model = joblib.load("churn_rf_model.joblib")
 encoder = joblib.load("encoder.joblib")
